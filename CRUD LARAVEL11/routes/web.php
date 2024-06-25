a6e70e6f1f88f668dc6a29c736959fd4072d90fc
Dimas Prasetio <dimas.prasetio@neuronworks.co.id>
2024-06-25 11:54:54 +0700

    update

--- CRUD LARAVEL11/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Namapasiencontroller;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::resource('Namapasien', Namapasiencontroller::class);
